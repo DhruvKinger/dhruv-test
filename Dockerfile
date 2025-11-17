fe3fbb356cc684f77c36f4288ad9475752edf97b
Dhruv Kinger <Dhruv.kinger@thrivepass.com>
2025-11-18 01:09:46 +0530

    commit

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-
-FROM dhruvreg.azurecr.io/azurefunctionsimage:v1.0.0
-
-ENV host:logger:consoleLoggingMode=always
-
-COPY . /home/site/wwwroot
-
-RUN cd /home/site/wwwroot && pip install -r requirements.txt
--- getPosts/__init__.py
+++ getPosts/__init__.py
@@ -2,6 +2,7 @@ import logging
 import azure.functions as func
 import pymongo
 import json
+import os
 from bson.json_util import dumps
 
 
@@ -10,7 +11,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python getPosts trigger function processed a request.')
 
     try:
-        url = "mongodb://cosmosaccountdhruv:fsivyka2LVu3xZfCUeMnk5kAaECSgIN7DZNp8XRlbZQnpHvDqThzkTz1s03dceV6EpL84U9IdsFYACDbIUlxGQ==@cosmosaccountdhruv.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmosaccountdhruv@"
+        url = os.getenv("MONGODB_CONNECTION_STRING", "mongodb://cosmosaccountdhruv:fsivyka2LVu3xZfCUeMnk5kAaECSgIN7DZNp8XRlbZQnpHvDqThzkTz1s03dceV6EpL84U9IdsFYACDbIUlxGQ==@cosmosaccountdhruv.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@cosmosaccountdhruv@")
         client = pymongo.MongoClient(url)
         database = client['db']
         collection = database['posts']
@@ -19,8 +20,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         result = dumps(result)
 
         return func.HttpResponse(result, mimetype="application/json", charset='utf-8', status_code=200)
-    except:
-        return func.HttpResponse("Bad request.", status_code=400)
     except Exception as e:
         logging.error('Operational failure in getPosts: %s\n%s', e, traceback.format_exc())
         return func.HttpResponse(f"Operational error: {e}", status_code=500)
